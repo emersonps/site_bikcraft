877d9bdb2bbe300ada8ac5a7accdabf52ebd82c5
Emerson Pinheiro de Souza <emersonline2007@gmail.com>
2020-12-13 23:00:02 -0300

    - Backend: Configuração de envio de formulário usando os plugins: PHPMailer e Sendgrid; - Otimizaçao do site para pesquisas no google - SEO.

--- js/script.js
+++ js/script.js
@@ -19,3 +19,11 @@ if(window.SimpleAnime){
   new SimpleAnime();
 }
 
+if(window.SimpleForm){
+  new SimpleForm({
+    form: ".formphp", // seletor do formulário
+    button: "#enviar", // seletor do botão
+    erro: "<div id='form-erro'><h2>Um erro ocorreu, tente para o email contato@bikcraft.com.</h2></div>", // mensagem de erro
+    sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
+  });
+}
